6db93a0396600bb009c6c3b34d4015d9a7a3e0a0
caroline_montagner <caroline.montagner@salux.com.br>
2024-03-18 19:48:02 -0300

    Adicionar arquivos de projeto.

---
+++ ServicoPrime.Teste/ServicoPrime_ValidaPrimeTeste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Prime.Servicos;
+
+namespace Prime.TesteUnitario.Servicos
+{
+    public class ServicoPrime_ValidaPrimeTeste
+    {
+        [Fact]
+        public void ValidaPrime_Input_RetornaFalso()
+        {
+            var servicoPrime = new ServicoPrime();
+            bool resultado = servicoPrime.ValidaPrime(1);
+            Assert.False(resultado, "Não sou cliente prime");
+        }
+    }
+}
---
+++ ServicoPrime/ServicoPrime.cs
@@ -0,0 +1,9 @@
+﻿namespace Prime.Servicos;
+
+public class ServicoPrime
+{
+    public bool ValidaPrime(int candidato)
+    {
+        throw new NotImplementedException("Não implementado");
+    }
+}
